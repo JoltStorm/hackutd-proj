5a086af5a0fee60f5875555e8fb50abd755bec6c
june <orcam20098@gmail.com>
2025-03-08 15:37:32 -0600

    cleaned up code for machine formatting

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <Resource resourceName="Oxygen" resourceColor="#007B9D" amountState="10.0 L" />
           <Resource resourceName="Water" resourceColor="#13039E" amountState="10.0 L" />
         </div>
-        <Machine/>
+        <Machine MachineName="Water Creation Unit" MachineQuantity={25}/>
       </div>
 		</>
 	)
