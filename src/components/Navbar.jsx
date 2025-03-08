b9c4e1f82ec94dd826810e3ffb96c2fa244ccb09
Enckripted <71646814+Enckripted@users.noreply.github.com>
2025-03-08 22:51:06 +0000

    multipage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,24 @@ import { React, useState } from 'react'
 import './Navbar.css'
 
 export default function Navbar(){
-    const [selected, setSelected] = useState(0)
+    const [selected, setSelected] = useState('spaceship')
+
+    function handleSelect(item){
+        setSelected(item)
+    }
 
     return (
         <div className = 'navbar flex'>
             <ul className = 'w-full'>
                 <li 
-                className={'list-element w-full mt-2 ' + (selected == 0 ? 'selected' : '')}
-                onClick={() => {setSelected(0)}}
+                className={`list-element w-full mt-2 ${(selected == 'spaceship' ? 'selected' : '')}`}
+                onClick={() => handleSelect('spaceship')}
                 >
                     <span className = 'm-2 text-white'>Spaceship</span>
                 </li>
                 <li 
-                className={'list-element w-full mt-2 ' + (selected == 1 ? 'selected' : '')}
-                onClick={() => { setSelected(1) }}
+                className={'list-element w-full mt-2 ' + (selected == 'planets' ? 'selected' : '')}
+                onClick={() => handleSelect('planets')}
                 >
                     <span className = 'm-2 text-white'>Planets</span>
                 </li>
