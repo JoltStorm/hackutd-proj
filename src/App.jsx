f9d78cedfcc6ad03f05412155912228c63089633
june <orcam20098@gmail.com>
2025-03-08 13:26:44 -0600

    basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+
+import React from 'react'
 import { useState } from 'react'
 
+
 function App(){
-  
+  return(
+    <div className="text-3xl flex justify-between">
+        <h1>test</h1>
+    </div>
+  )
 }
+
+export default App
