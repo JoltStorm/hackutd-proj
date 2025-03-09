7429bfa79bce7179aadbcdf4ad856c5317fecebe
Enckripted <diegoemorales1@gmail.com>
2025-03-08 20:47:17 -0600

    Rewriting machine component

--- src/components/Machine.jsx
+++ src/components/Machine.jsx
@@ -3,37 +3,47 @@ import { React, useState } from 'react'
 import './Machine.css'
 
 
-export default function WaterCreationUnit({ MachineName, MachineQuantity }) {
-  const [unitsBuilt, setUnitsBuilt] = useState(MachineQuantity);
+export default function Machine({ machineName, machineQuantity, inputResources, outputResources, costResources }) {
+	const [unitsBuilt, setUnitsBuilt] = useState(machineQuantity);
 
-  return (
-    <div className=" text-white w-96">
-      <div className="bg-gray-300 p-4 text-black rounded-md">
-        <h2 className="text-xl">{MachineName}</h2>
+	const inputs = inputResources.map(resource =>
+		<div key={resource.color} style={{ backgroundColor: resource.color }} className="text-white text-sm w-40">-{resource.usage * machineQuantity} L/s (-{resource.usage}/s indv)</div>
+	)
+	const outputs = outputResources.map(resource =>
+		<div key={resource.color} style={{ backgroundColor: resource.color }} className="text-white text-sm w-40">+{resource.usage * machineQuantity} L/s (+{resource.usage}/s indv)</div>
+	)
+	const costs = costResources.map(resource =>
+		<div key={resource.color} style={{ backgroundColor: resource.color }} className="text-white text-sm w-20">{resource.usage} g</div>
+	)
 
-        <p className="text-xl">Input</p>
-        <div className="bg-red-600 text-white px-2 py-1 text-sm w-40">-250 L/s (-10/s indv)</div>
-        <div className="bg-cyan-600 text-white px-2 py-1 text-sm w-40">-125 L/s (-5/s indv)</div>
-        <p className="text-xl">Output</p>
-        <div className="bg-blue-700 text-white px-2 py-1 text-sm w-40">+125 L/s (+5/s indv)</div>
+	return (
+		<div className=" text-white w-96">
+			<div className="bg-gray-300 p-4 text-black rounded-md">
+				<h2 className="text-xl">{machineName}</h2>
 
-        <p>Cost to build unit: 50</p>
-        <p>Units built: {unitsBuilt}</p>
-        <div className="flex gap-2">
-          <button
-            className="bg-green-600 text-white px-4 py-2 rounded-md"
-            onClick={() => setUnitsBuilt(unitsBuilt + 1)}
-          >
-            Build
-          </button>
-          <button
-            className="bg-red-600 text-white px-4 py-2 rounded-md"
-            onClick={() => setUnitsBuilt(unitsBuilt > 0 ? unitsBuilt - 1 : 0)}
-          >
-            Remove
-          </button>
-        </div>
-      </div>
-    </div>
-  )
+				<p className="text-xl">Input</p>
+				{inputs}
+				<p className="text-xl">Output</p>
+				{outputs}
+				<p className="text-xl">Cost</p>
+				{costs}
+
+				<p>Units built: {unitsBuilt}</p>
+				<div className="flex gap-2">
+					<button
+						className="bg-green-600 text-white px-4 py-2 rounded-md"
+						onClick={() => setUnitsBuilt(unitsBuilt + 1)}
+					>
+						Build
+					</button>
+					<button
+						className="bg-red-600 text-white px-4 py-2 rounded-md"
+						onClick={() => setUnitsBuilt(unitsBuilt > 0 ? unitsBuilt - 1 : 0)}
+					>
+						Remove
+					</button>
+				</div>
+			</div>
+		</div>
+	)
 }
