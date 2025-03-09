ee83198a52a993677f741edb66c3ca2387bbbee6
Enckripted <diegoemorales1@gmail.com>
2025-03-08 23:04:25 -0600

    saving

---
+++ src/components/context/EarthContext.jsx
@@ -0,0 +1,25 @@
+import { useContext } from 'react'
+import { createContext } from 'react'
+
+export const EarthContext = createContext()
+
+export const EarthProvider = ({ children }) => {
+    const [state, setState] = useState({
+        resources: {
+            hydrogen: { name: "Hydrogen", color: "red", amount: 0 },
+            oxygen: { name: "Oxygen", color: "red", amount: 0 },
+            water: { name: "Water", color: "red", amount: 0 }
+        },
+        machines: {}
+    })
+
+    return (
+        <GameContext.Provider value={{ state }}>
+            {children}
+        </GameContext.Provider>
+    )
+}
+
+export const useGameContext = () => {
+    return useContext(GameContext)
+}
